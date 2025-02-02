f82ea13b2f280cf2a37f0a6a090a5b537915ab29
greeneggs1 <amir.ali9667@gmail.com>
2025-02-02 11:43:24 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ProjectDecisionTool } from './components/ProjectDecisionTool';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <ProjectDecisionTool />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
